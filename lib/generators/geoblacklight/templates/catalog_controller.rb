65c68eae87bff97549497f49ea175162d4af1b38
yjyale <yue.ji@yale.edu>
2020-12-08 11:35:05 -0500

    Initial changes

--- lib/generators/geoblacklight/templates/catalog_controller.rb
+++ lib/generators/geoblacklight/templates/catalog_controller.rb
@@ -262,7 +262,8 @@ class CatalogController < ApplicationController
     # 'openstreetmapHot'
     # 'openstreetmapStandard'
 
-    config.basemap_provider = 'positron'
+    #config.basemap_provider = 'positron'
+    config.basemap_provider = 'openstreetmapHot'
 
     # Configuration for autocomplete suggestor
     config.autocomplete_enabled = true
